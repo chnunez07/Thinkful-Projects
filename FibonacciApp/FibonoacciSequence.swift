4478201979330794446769eee528d89058e7d22c
chnunez07 <chnunez@chriss-air.lan>
2016-11-22 18:25:22 +0100

    resolved 32-bit challenge

---
+++ FibonacciApp/FibonoacciSequence.swift
@@ -0,0 +1,97 @@
+//
+//  Fibanoacci.swift
+//  FibonacciApp
+//
+//  Created by Chris on 11/17/16.
+//  Copyright © 2016 CHN. All rights reserved.
+//
+
+import Foundation
+
+class FibonacciSequence {
+    
+    let includesZero: Bool
+    let values: [UInt]
+    
+    init(maxNumber: UInt, includesZero: Bool)
+    {
+        self.includesZero = includesZero
+        if maxNumber == 0 && includesZero == false
+        {
+            values = []
+        }
+        else if maxNumber == 0
+        {
+            values = [0]
+        }
+        else
+        {
+            var sequence: [UInt] = [0,1,1]
+            var nextNumber: UInt = 2
+            while nextNumber <= maxNumber
+                {
+                sequence.append(nextNumber)
+                let lastNumber = sequence.last!
+                let secondToLastNumber = sequence[sequence.count-2]
+                let (sum, didOverflow) = UInt.addWithOverflow(lastNumber, secondToLastNumber)
+                if didOverflow == true
+                    {
+                    print("Overflow! The next number is too big to store in a UInt!")
+                    break
+                    }
+                nextNumber = sum
+                }
+            if includesZero == false
+                {
+                sequence.remove(at:0)
+                }
+                values = sequence
+        }
+    }
+    
+    init(numberOfItemsInSequence: UInt, includesZero: Bool)
+        {
+        self.includesZero = includesZero
+        if numberOfItemsInSequence == 0
+            {
+            values = []
+            }
+        else if numberOfItemsInSequence == 1
+        {
+            if includesZero == true
+            {
+                values = [0]
+            }
+            else
+            {
+                values = [1]
+            }
+        }
+        else
+        {
+            var sequence: [UInt]
+            if includesZero == true
+            {
+                sequence = [0,1]
+            }
+            else
+            {
+                sequence = [1,1]
+            }
+            for _ in 2 ..< numberOfItemsInSequence
+            {
+                let lastNumber = sequence.last!
+                let secondToLastNumber = sequence[sequence.count-2]
+                let (nextNumber, didOverflow) = UInt.addWithOverflow(lastNumber, secondToLastNumber)
+                if didOverflow == true {
+                    print("Overflow! The next number is too big to store in a UInt!")
+                    break
+            }
+                sequence.append(nextNumber)
+            }
+        values = sequence
+        }
+    }
+}
+
+
--- FibonacciApp/ViewController.swift
+++ FibonacciApp/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import DeviceKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    var fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: 2, includesZero: true)
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.updateFibonacciSequence()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+   
     }
-
-
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var numberOfItemsSlider: UISlider!
+    @IBOutlet weak var includesZeroSwitch: UISwitch!
+    @IBOutlet weak var includesZeroLabel: UILabel!
+    @IBOutlet weak var numberOfItemsLabel: UILabel!
+    
+    @IBAction func updateFibonacciSequence()
+    {
+    
+        let device = Device()
+        print(device) // prints, for example, "iPhone 6 Plus"
+        
+        let groupOfAllowedDevices: [Device] = [.iPhone5s, .iPhone6, .iPhone6Plus, .iPhone6s, .iPhone6sPlus,.simulator(.iPhone5s), .simulator(.iPhone6), .simulator(.iPhone6Plus), .simulator(.iPhone6s), .simulator(.iPhone6sPlus)]
+        
+        if device.isOneOf(groupOfAllowedDevices) {
+            
+        if includesZeroSwitch.isOn
+        {
+            numberOfItemsSlider.maximumValue = 94
+        }
+        else
+        {
+            numberOfItemsSlider.maximumValue = 93
+        }
+    }
+    else
+    {
+        if includesZeroSwitch.isOn
+        {
+            numberOfItemsSlider.maximumValue = 48
+        }
+        else
+        {
+            numberOfItemsSlider.maximumValue = 47
+        }
+    }
+    fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: UInt(numberOfItemsSlider.value), includesZero: includesZeroSwitch.isOn)
+    
+    textView.text = fibonacciSequence.values.description
+    
+    numberOfItemsLabel.text = String(Int(numberOfItemsSlider.value))
+    
+    if includesZeroSwitch.isOn == true
+    {
+    includesZeroLabel.text = "Yes"
+    }
+    
+    else
+    {
+    includesZeroLabel.text = "No"
+    }
+        
+    }
+    
 }
-
